29c1c2104c6729e278c606e27b39bfd30c652fb7
Yousef Abuali <abualiyousef@outlook.com>
2024-03-12 16:55:31 +0200

    Add 'Id' property

--- SIMS/SIMS/Product.cs
+++ SIMS/SIMS/Product.cs
@@ -1,15 +1,14 @@
 namespace SIMS;
 
-public class Product
+public class Product(int id, string? name, decimal? price, int? quantity)
 {
-    public string? Name { get; set; }
-    public decimal? Price { get; set; }
-    public int? Quantity { get; set; }
-    
-    public Product(string? name, decimal? price, int? quantity)
+    public int Id { get; set; } = id;
+    public string? Name { get; set; } = name;
+    public decimal? Price { get; set; } = price;
+    public int? Quantity { get; set; } = quantity;
+
+    public override string ToString()
     {
-        Name = name;
-        Price = price;
-        Quantity = quantity;
+        return $"ID: {Id}, Name: {Name}, Price: {Price}, Quantity: {Quantity}";
     }
-} 
+}
