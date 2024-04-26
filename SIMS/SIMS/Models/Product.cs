7fd15daf1091cb06c73683228b31464cb71b7acd
Yousef Abuali <abualiyousef@outlook.com>
2024-04-26 04:10:51 +0300

    Refactor Product.cs

--- SIMS/SIMS/Models/Product.cs
+++ SIMS/SIMS/Models/Product.cs
@@ -2,10 +2,10 @@ namespace SIMS.Models;
 
 public class Product(string? name, decimal? price, int? quantity)
 {
-    public int Id { get;}
-    public string? Name { get; } = name;
-    public decimal? Price { get;} = price;
-    public int? Quantity { get;} = quantity;
+    public int Id { get; set; }
+    public string? Name { get; private set; } = name;
+    public decimal? Price { get; private set; } = price;
+    public int? Quantity { get; private set; } = quantity;
 
     public override string ToString()
     {
