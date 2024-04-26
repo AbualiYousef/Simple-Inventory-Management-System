b641e7b4a0365a946a7c273c238e7ec383f9857b
Yousef Abuali <abualiyousef@outlook.com>
2024-04-26 05:39:34 +0300

    Implement Inventory.cs

--- SIMS/SIMS/Inventory.cs
+++ SIMS/SIMS/Inventory.cs
@@ -1,49 +1,49 @@
+using MongoDB.Bson;
 using SIMS.Exceptions;
 using SIMS.Models;
 using SIMS.Repositories;
 
 namespace SIMS;
 
-public class Inventory(string connectionString)
+public class Inventory(string connectionString, string databaseName)
 {
-    private readonly SqlProductRepository _sqlProductRepository = new(connectionString);
+    private readonly MongoProductRepository _mongoProductRepository = new(connectionString, databaseName);
 
     public async Task AddProductAsync(Product product)
     {
         ArgumentNullException.ThrowIfNull(product);
-        await _sqlProductRepository.AddProductAsync(product);
+        await _mongoProductRepository.AddProductAsync(product);
     }
 
     public async Task<List<Product>> GetAllProductsAsync()
     {
-        return await _sqlProductRepository.GetAllProductsAsync();
+        return await _mongoProductRepository.GetAllProductsAsync();
     }
 
-    public async Task UpdateProductAsync(int id, Product product)
+    public async Task UpdateProductAsync(ObjectId id, Product product)
     {
         ArgumentNullException.ThrowIfNull(product);
-
-        if (!await _sqlProductRepository.ProductExistsAsync(id))
+        if (!await _mongoProductRepository.ProductExistsAsync(id))
         {
             throw new ProductNotFoundException();
         }
 
-        await _sqlProductRepository.UpdateProductAsync(id, product);
+        await _mongoProductRepository.UpdateProductAsync(id, product);
     }
 
-    public async Task<bool> DeleteProductAsync(int id)
+    public async Task<bool> DeleteProductAsync(ObjectId id)
     {
-        if (!await _sqlProductRepository.ProductExistsAsync(id))
+        if (!await _mongoProductRepository.ProductExistsAsync(id))
         {
             throw new ProductNotFoundException();
         }
 
-        return await _sqlProductRepository.DeleteProductAsync(id);
+        return await _mongoProductRepository.DeleteProductAsync(id);
     }
 
     public async Task<Product> SearchProductByNameAsync(string name)
     {
-        var product = await _sqlProductRepository.GetProductByNameAsync(name);
+        var product = await _mongoProductRepository.GetProductByNameAsync(name);
         if (product is null)
         {
             throw new ProductNotFoundException();
