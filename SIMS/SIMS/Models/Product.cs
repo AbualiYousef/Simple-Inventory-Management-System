406a1be0b6c8e47e62dc57f2e95fa06b2deb8d90
Yousef Abuali <abualiyousef@outlook.com>
2024-04-26 05:39:58 +0300

    Refactor Product.cs to use in mongoDb

--- SIMS/SIMS/Models/Product.cs
+++ SIMS/SIMS/Models/Product.cs
@@ -1,10 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace SIMS.Models;
 
 public class Product(string? name, decimal? price, int? quantity)
 {
-    public int Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    [BsonElement("name")]
     public string? Name { get; private set; } = name;
+
+    [BsonElement("price")]
     public decimal? Price { get; private set; } = price;
+
+    [BsonElement("quantity")]
     public int? Quantity { get; private set; } = quantity;
 
     public override string ToString()
